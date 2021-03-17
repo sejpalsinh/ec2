203e2bc2a893ba225e4dad4e80200d6d8b4ce37e
sejpalsinh jadeja <59164285+sejpalsinh@users.noreply.github.com>
2021-03-17 19:36:24 +0530

    Delete login.py

--- login.py
+++
@@ -1,5 +0,0 @@
-from kiteconnect import KiteConnect
-api_key = "ky3cmqfglvl58il2"
-access_token = "FdsloVVdWLsfHpYUWNavT1JjFpd29ogj"
-kite = KiteConnect(api_key=api_key)
-kite.set_access_token(access_token)
